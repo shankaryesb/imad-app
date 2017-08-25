ef3fd3aa5bac7007cac8244da5cb1a9a7298805c
shankaryesb <30768498+shankaryesb@users.noreply.github.com>
2017-08-25 16:23:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,15 +8,19 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'shankar-1.html'));
 });
+
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'shankar-2.html'));
 });
+
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'shankar-3.html'));
 });
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
