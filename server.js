dc53ecfbb32e40665585e140f466423ac8182a96
shankaryesb <30768498+shankaryesb@users.noreply.github.com>
2017-08-25 16:18:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'shankar-2.html'));
 });
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'shankar-1.html'));
+    res.sendFile(path.join(__dirname, 'ui', 'shankar-3.html'));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
